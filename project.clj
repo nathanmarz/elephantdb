26931604d9298e807be9ded53df3a96608d492ba
Nathan Marz <nathan.marz@gmail.com>
2011-02-15 17:41:55 -0800

    new project.clj for elephantdb

---
+++ project.clj
@@ -0,0 +1,21 @@
+(defproject elephantdb/elephantdb "0.0.4"
+  :source-path "src/clj"
+  :test-path "test/clj"
+  :java-source-path "src/jvm"
+  :javac-options {:debug "true" :fork "true"}
+  :repositories {
+                 "oracle" "http://download.oracle.com/maven"
+                 }
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [jvyaml "1.0.0"]
+                 [backtype/thriftjava "1.0.0"]
+                 [log4j/log4j "1.2.16"]
+                 [com.sleepycat/je "4.1.6"]
+                 ]
+  :dev-dependencies [
+                     [org.apache.hadoop/hadoop-core "0.20.2-dev"]
+                     [swank-clojure "1.2.1"]
+                    ]
+  :aot :all
+)
