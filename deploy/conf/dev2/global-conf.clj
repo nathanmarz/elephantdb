1e1ccf7f8de0c62674619674c8e8d9b3ac5dd199
Jason Jackson <jason@cvk.ca>
2011-04-26 18:11:10 -0700

    config files

---
+++ deploy/conf/dev2/global-conf.clj
@@ -0,0 +1,11 @@
+{
+ :replication 1
+ :node-count 1
+ :port 3578
+ :domains {
+           "tweet-lists" "s3n://hdfs2/data/lyoto/output/customer-stats/tweet-lists" ;;1
+           "tweet-stats" "s3n://hdfs2/data/lyoto/output/customer-stats/tweet-stats" ;;1
+           "user-day-lists" "s3n://hdfs2/data/lyoto/output/customer-stats/user-day-lists" ;;1
+           "user-day-stats" "s3n://hdfs2/data/lyoto/output/customer-stats/user-day-stats" ;;1
+           }
+}
---
+++ deploy/conf/dev2/local-conf.clj
@@ -0,0 +1,7 @@
+{ :local-dir "/mnt/elephantdb"
+  :max-online-download-rate-kb-s 1024
+  :local-db-conf {"elephantdb.JavaBerkDB" {}
+                  "elephantdb.TokyoCabinet" {}
+                  }
+  :hdfs-conf {"fs.default.name" "s3n://hdfs2"}
+}
