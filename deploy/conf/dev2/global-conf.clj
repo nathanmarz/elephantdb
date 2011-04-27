8637f0ea3b710473f002955c48ee3e222351b49b
Jason Jackson <jason@cvk.ca>
2011-04-26 19:57:31 -0700

    cleaned up example config

--- deploy/conf/dev2/global-conf.clj
+++ deploy/conf/dev2/global-conf.clj
@@ -3,9 +3,6 @@
  :node-count 1
  :port 3578
  :domains {
-           "tweet-lists" "s3n://hdfs2/data/lyoto/output/customer-stats/tweet-lists" ;;1
-           "tweet-stats" "s3n://hdfs2/data/lyoto/output/customer-stats/tweet-stats" ;;1
-           "user-day-lists" "s3n://hdfs2/data/lyoto/output/customer-stats/user-day-lists" ;;1
-           "user-day-stats" "s3n://hdfs2/data/lyoto/output/customer-stats/user-day-stats" ;;1
+           "domain-name" "s3n://hdfs2/path/.../"
            }
 }
