08988f232e77189dc0a25755075035acb8fbbd06
Nathan Marz <nathan.marz@gmail.com>
2011-06-05 16:01:22 -0700

    fix edb deploy project.clj

--- deploy/project.clj
+++ deploy/project.clj
@@ -5,27 +5,15 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
 
-                 [org.cloudhoist/pallet "0.5.0-SNAPSHOT"
-                  :exclusions [org.jclouds/jclouds-jsch
-                               org.jclouds/jclouds-log4j
-                               org.jclouds.driver/jclouds-jsch
-                               org.jclouds.driver/jclouds-log4j
-                               org.jclouds.driver/jclouds-enterprise
-                               org.jclouds/jclouds-blobstore
-                               org.jclouds/jclouds-bouncycastle
-                               org.jclouds.driver/jclouds-bouncycastle
-                               org.jclouds/jclouds-compute
-                               org.jclouds/jclouds-scriptbuilder
-                               ]]
-                 [org.cloudhoist/java "0.5.0-SNAPSHOT"]
-                 [org.cloudhoist/git "0.5.0-SNAPSHOT"]
-                 [org.cloudhoist/ssh-key "0.5.0-SNAPSHOT"]
-                 [org.cloudhoist/automated-admin-user "0.5.0-SNAPSHOT"]
+                 [org.cloudhoist/pallet "0.5.0"]
+                 [org.cloudhoist/java "0.5.0"]
+                 [org.cloudhoist/git "0.5.0"]
+                 [org.cloudhoist/ssh-key "0.5.0"]
+                 [org.cloudhoist/automated-admin-user "0.5.0"]
 
-                 [org.jclouds.provider/aws-ec2 "1.0-SNAPSHOT"]
-                 [org.jclouds.provider/aws-s3 "1.0-SNAPSHOT"]
+                 [org.jclouds.provider/aws-ec2 "1.0-beta-9c"]
+                 [org.jclouds.provider/aws-s3 "1.0-beta-9c"]
 
-                 [org.jclouds.driver/jclouds-jsch "1.0-SNAPSHOT"]
                  [com.jcraft/jsch "0.1.44-1"]
 
                  [org.antlr/stringtemplate "3.2"]]
