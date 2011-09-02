f1d667738729ad1982fb545038c0cdae8e6ec6de
Sam Ritchie <sritchie@twitter.com>
2011-09-02 10:31:16 -0700

    removed conf directory.

--- deploy/conf/example/global-conf.clj
+++
@@ -1,8 +0,0 @@
-{
- :replication 1
- :node-count 1
- :port 3578
- :domains {
-           "domain-name" "s3n://hdfs2/path/.../"
-           }
- }
--- deploy/conf/example/local-conf.clj
+++
@@ -1,5 +0,0 @@
-{:local-dir "/mnt/elephantdb"
- :max-online-download-rate-kb-s 1024
- :local-db-conf {"elephantdb.JavaBerkDB" {}
-                 "elephantdb.TokyoCabinet" {}}
- :hdfs-conf {"fs.default.name" "s3n://<yourbasebucket>"}}
