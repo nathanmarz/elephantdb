0a6eae82bc654b505b3af5003807f7aeb401dc3b
Sam Ritchie <sritchie@twitter.com>
2011-09-21 10:26:08 -0700

    moved deploy to separate project.

--- deploy/dist/classpath.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-CLASSPATH="$(find lib/ -follow -mindepth 1 -print0 2> /dev/null | tr \\0 \:)"
-
-echo $CLASSPATH
--- deploy/dist/project.clj
+++
@@ -1,3 +0,0 @@
-(defproject elephantdb-deploy "1.1.0"
-  :dependencies [[elephantdb "0.0.8-SNAPSHOT"]
-                 [org.apache.hadoop/hadoop-core "0.20.2-dev"]])
