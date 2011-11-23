c8d7098a8fcba6b83b4a8c116b0ed29a8d6488ff
Sam Ritchie <sritchie09@gmail.com>
2011-11-22 19:07:51 -0500

    notes on sharder and transmitter.

--- src/jvm/elephantdb/persistence/Sharder.java
+++ src/jvm/elephantdb/persistence/Sharder.java
@@ -1,6 +1,13 @@
 package elephantdb.persistence;
 
-/** User: sritchie Date: 11/22/11 Time: 5:27 PM */
+/** User: sritchie Date: 11/22/11 Time: 5:27 PM
+ *
+ * A Sharder takes in key and value objects and returns a shard index. Search, for example;
+ * A LuceneSharder might be configured to shard against the "user" field in a document (value).
+ *
+ * For lucene, the key doesn't really matter at all. In wonderdog it's null, actually.
+ *
+ * */
 public interface Sharder {
     public int shardIndex(int numShards, Object key, Object val);
 }
--- src/jvm/elephantdb/persistence/Transmitter.java
+++ src/jvm/elephantdb/persistence/Transmitter.java
@@ -1,6 +1,14 @@
 package elephantdb.persistence;
 
-/** User: sritchie Date: 11/22/11 Time: 4:01 PM */
+/** User: sritchie Date: 11/22/11 Time: 4:01 PM
+ *
+ * The transmitter deals with serialization and deserialization of keys and values.
+ * After serialization, keys and vals are passed through the hadoop machinery, where they
+ * reappear as calls to the ElephantUpdater.
+ *
+ * For search, the updater will most likely only be adding documents to a given shard. For
+ * key-value, the updater might need to perform a get and merge values.
+ * */
 public interface Transmitter {
     public byte[] serializeKey(Object key);
     public byte[] serializeVal(Object val);
