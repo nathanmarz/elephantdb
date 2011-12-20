af84ea7cce7e9f235dcbb20174140c5a628c58df
Sam Ritchie <sritchie09@gmail.com>
2011-12-19 19:32:37 -0500

    Cleaner dependencies.

--- project.clj
+++ project.clj
@@ -6,21 +6,12 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [jvyaml "1.0.0"]
                  [backtype/thriftjava "1.0.0"]
-
                  [jackknife "0.1.0"]
-                 
-                 ;; TODO: Switch to Jackknife logging.
-                 ;; [org.clojure/tools.logging "0.2.3"]
-                 ;; [log4j/log4j "1.2.16"]
-                 [hadoop-util "0.2.3"]
+                 [hadoop-util "0.2.4"]
                  [cascading.kryo "0.1.5"]
-
-                 ;; Break out to separate key-value and search projects.
                  [com.sleepycat/je "4.1.10"]
                  [org.apache.lucene/lucene-core "3.0.3"]
                  [org.apache.lucene/lucene-queries "3.0.3"]]
   :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]
-                     [lein-marginalia "0.6.1"]
-                     [midje "1.3.0"]
-                     [lein-midje "1.0.4"]]
+                     [midje "1.3.0"]]
   :main elephantdb.keyval.main)
