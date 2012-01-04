3f03af2c357959ad7ffc0e3b61cf142139ecaff1
Sam Ritchie <sritchie09@gmail.com>
2012-01-03 16:03:24 -0800

    Better spec-test.

--- test/elephantdb/common/spec_test.clj
+++ test/elephantdb/common/spec_test.clj
@@ -1,30 +1,32 @@
 (ns elephantdb.common.spec-test
-  (:use clojure.test)
-  (:import [elephantdb DomainSpec]
+  (:use midje.sweet)
+  (:import [elephantdb DomainSpec Utils]
            [elephantdb.document KeyValDocument]))
 
 (defn domain-spec
   [coord-class shard-scheme shard-count]
   (DomainSpec. coord-class shard-scheme shard-count))
 
-(def test-spec
-  (domain-spec "elephantdb.persistence.JavaBerkDB"
-               "elephantdb.partition.HashModScheme"
-               2))
-
 (defn kv-doc [k v]
   (KeyValDocument. k v))
 
-(defn doc= [x y]
-  (and (= (.key x) (.key y))
-       (= (.value x) (.value y))))
-
-(deftest round-trip-test
-  (are [k v] (let [original (kv-doc k v)
-                   round-tripped (->> original
-                                      (.serialize test-spec)
-                                      (.deserialize test-spec))]
-               (doc= original round-tripped))
-       1       2
-       "three" "four!"))
+(defn round-trip [item]
+  (let [serializer (Utils/makeSerializer
+                    (domain-spec "elephantdb.persistence.JavaBerkDB"
+                                 "elephantdb.partition.HashModScheme"
+                                 2))]
+    (->> item
+         (.serialize serializer)
+         (.deserialize serializer))))
+(tabular
+ (fact
+   "Key Value documents should be able to be round-tripped through a
+    serializer."
+   (let [doc           (kv-doc ?key ?val)
+         round-tripped (round-trip doc)]
+     (.key doc)   => (.key round-tripped)
+     (.value doc) => (.value round-tripped)))
+ ?key    ?val
+ 1       2
+ "three" "four!")
 
