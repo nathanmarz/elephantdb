6698c5f329ef54af716fea197877513dc8a0eaa3
Soren Macbeth <soren@yieldbot.com>
2012-04-30 20:30:03 -0400

    use cloudera repo and chd3 hadoop jars for deps

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :java-source-path "src/jvm"
   :javac-options {:debug "true" :fork "true"}
   :jvm-opts ["-Xmx768m" "-server"]
-  :repositories {"oracle" "http://download.oracle.com/maven"}
+  :repositories {"oracle" "http://download.oracle.com/maven"
+                 "cloudera" "https://repository.cloudera.com/artifactory/cloudera-repos"}
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [jvyaml "1.0.0"]
                  [org.yaml/snakeyaml "1.9"]
@@ -18,9 +19,9 @@
                  [cascalog/carbonite "1.1.0"]
                  [com.sleepycat/je "5.0.34"]
                  [org.apache.lucene/lucene-core "3.0.3"]
-                 [org.apache.lucene/lucene-queries "3.0.3"]]
-  :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]
-                     [midje "1.3.1" :exclusions [org.clojure/clojure]]
+                 [org.apache.lucene/lucene-queries "3.0.3"]
+                 [org.apache.hadoop/hadoop-core "0.20.2-cdh3u3"]]
+  :dev-dependencies [[midje "1.3.1" :exclusions [org.clojure/clojure]]
                      [lein-midje "1.0.8"]]
   :main elephantdb.keyval.core)
 
