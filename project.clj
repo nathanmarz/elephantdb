d5d341497635bfeeaff0a713e9bc6cda906a6304
Soren Macbeth <soren@dopeness.org>
2012-08-15 10:21:40 -0700

    bump to SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject yieldbot/elephantdb "0.2.0-wip9"
+(defproject yieldbot/elephantdb "0.2.0-SNAPSHOT"
   :source-path "src/clj"
   :java-source-path "src/jvm"
   :javac-options {:debug "true" :fork "true"}
