abf14c26d48f88e1ba9995bfbad3373835ee0a98
Soren Macbeth <soren@dopeness.org>
2013-01-07 14:22:19 -0800

    remove fixed buffer size in kryo serializer

--- src/jvm/elephantdb/serialize/KryoSerializer.java
+++ src/jvm/elephantdb/serialize/KryoSerializer.java
@@ -9,6 +9,8 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.log4j.Logger;
 
 import java.util.ArrayList;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 /**
  * User: sritchie
@@ -17,20 +19,10 @@ import java.util.ArrayList;
  */
 public class KryoSerializer implements Serializer {
     public static final Logger LOG = Logger.getLogger(KryoSerializer.class);
-
-    private static final int OUTPUT_BUFFER_SIZE = 1<<12;
-    private static final int MAX_OUTPUT_BUFFER_SIZE = 1<<24;
-
-    private static final int TIDY_FACTOR = 1<<4;
-
-    private static final int SWITCH_LIMIT = Math.max(MAX_OUTPUT_BUFFER_SIZE, MAX_OUTPUT_BUFFER_SIZE / TIDY_FACTOR);
-
-    private static final ThreadLocal<Output> output = new ThreadLocal<Output>();
+    
     private static final ThreadLocal<Kryo> kryo = new ThreadLocal<Kryo>();
-
-    private int prevPosition;
-
     private Iterable<KryoFactory.ClassPair> kryoPairs = new ArrayList<KryoFactory.ClassPair>();
+    private ByteArrayOutputStream byteStream;
 
     public KryoSerializer() {
     }
@@ -39,24 +31,10 @@ public class KryoSerializer implements Serializer {
         setKryoPairs(kryoPairs);
     }
 
-    public void tidyBuffer() {
-        int currentPosition = getOutput().position();
-
-        // If the previous serialized object was large (greater than the switch size) and the current
-        // object falls below the switch size, reset the buffer to be small again. If both objects
-        // are small (or large), no reallocation occurs.
-        if (prevPosition > SWITCH_LIMIT && currentPosition <= SWITCH_LIMIT) {
-            getOutput().setBuffer(new byte[OUTPUT_BUFFER_SIZE], MAX_OUTPUT_BUFFER_SIZE);
-        }
-
-        prevPosition = currentPosition;
-        getOutput().clear();
-    }
-
     private Kryo freshKryo() {
         Kryo k = new Kryo();
+        byteStream = new ByteArrayOutputStream();
         KryoFactory factory = new KryoFactory(new Configuration());
-
         k.setInstantiatorStrategy(new StdInstantiatorStrategy());
         k.setRegistrationRequired(false);
         factory.registerBasic(k, getKryoPairs());
@@ -78,29 +56,17 @@ public class KryoSerializer implements Serializer {
         return kryo.get();
     }
 
-    public Output getOutput() {
-        if (output.get() == null)
-            output.set(new Output(OUTPUT_BUFFER_SIZE, MAX_OUTPUT_BUFFER_SIZE));
-
-        return output.get();
-    }
-
     public byte[] serialize(Object o) {
-        LOG.debug("Serializing object: " + o);
-        LOG.debug("Registration for " + o + " is: " + getKryo().getRegistration(o.getClass()));
-        Output output = getOutput();
-        getKryo().writeClassAndObject(output, o);
-        byte[] bytes =  output.toBytes();
-
-        tidyBuffer();
-
-        LOG.debug("returning " + bytes.length + " serialized bytes");
+        byteStream.reset();
+        Output ko = new Output(byteStream);
+        getKryo().writeClassAndObject(ko, o);
+        ko.flush();
+        byte[] bytes = ko.toBytes();
+        
         return bytes;
-
     }
 
     public Object deserialize(byte[] bytes) {
-        LOG.debug("deserializing " + bytes.length + " bytes");
         return getKryo().readClassAndObject(new Input(bytes));
     }
 
