8c2318aa2b310811ed1893ffe4467b2b7e6e2d85
Soren Macbeth <soren@dopeness.org>
2013-02-07 16:01:12 -0800

    bump to 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject elephantdb "0.2.0-SNAPSHOT"
+(defproject elephantdb "0.2.0"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [jvyaml "1.0.0"]
