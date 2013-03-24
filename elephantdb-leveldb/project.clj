741b4ba5ec76cc25bebcd3125d6d7a405704656b
Soren Macbeth <soren@dopeness.org>
2013-03-23 22:51:22 -0700

    leveldbjni 1.5

--- elephantdb-leveldb/project.clj
+++ elephantdb-leveldb/project.clj
@@ -4,7 +4,7 @@
   :javac-options ["-source" "1.6" "-target" "1.6"]
   :repositories {"fusesource.nexus.snapshot" "http://repo.fusesource.com/nexus/content/groups/public-snapshots"}
   :dependencies [[elephantdb/elephantdb-core "0.4.0-RC1"]
-                 [org.fusesource.leveldbjni/leveldbjni-all "1.4"]]
+                 [org.fusesource.leveldbjni/leveldbjni-all "1.5"]]
   :profiles {:dev
              {:dependencies
               [[midje "1.5.0"]]
