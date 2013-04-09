22d573452785472c00ffc2f2e68b07dd3d83c781
Soren Macbeth <soren@dopeness.org>
2013-04-08 20:37:35 -0700

    bump to snapshot

--- elephantdb-bdb/project.clj
+++ elephantdb-bdb/project.clj
@@ -1,9 +1,9 @@
-(defproject elephantdb/elephantdb-bdb "0.4.1"
+(defproject elephantdb/elephantdb-bdb "0.4.2-SNAPSHOT"
   :min-lein-version "2.0.0"
   :java-source-paths ["src/jvm"]
   :javac-options ["-source" "1.6" "-target" "1.6"]
   :repositories {"oracle" "http://download.oracle.com/maven"}
-  :dependencies [[elephantdb/elephantdb-core "0.4.1"]
+  :dependencies [[elephantdb/elephantdb-core "0.4.2-SNAPSHOT"]
                  [com.sleepycat/je "5.0.58"]]
   :profiles {:dev
              {:dependencies
--- elephantdb-server/project.clj
+++ elephantdb-server/project.clj
@@ -1,12 +1,12 @@
-(defproject elephantdb/elephantdb-server "0.4.1"
+(defproject elephantdb/elephantdb-server "0.4.2-SNAPSHOT"
   :min-lein-version "2.0.0"
   :source-paths ["src/clj"]
   :jvm-opts ["-Xmx768m" "-server" "-Djava.net.preferIPv4Stack=true" "-XX:+UseCompressedOops"]
   :dependencies [[org.slf4j/slf4j-log4j12 "1.6.4"]
                  [com.yammer.metrics/metrics-graphite "2.2.0"]
-                 [elephantdb/elephantdb-core "0.4.1"]
-                 [elephantdb/elephantdb-bdb "0.4.1"]
-                 [elephantdb/elephantdb-leveldb "0.4.1"]]
+                 [elephantdb/elephantdb-core "0.4.2-SNAPSHOT"]
+                 [elephantdb/elephantdb-bdb "0.4.2-SNAPSHOT"]
+                 [elephantdb/elephantdb-leveldb "0.4.2-SNAPSHOT"]]
   :profiles {:provided
              {:dependencies
               [[org.apache.hadoop/hadoop-core "0.20.2"]]}
