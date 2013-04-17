bd75cf20b5a1eddd2c7e3f2d60e6a3fbdc4529c8
Soren Macbeth <soren@dopeness.org>
2013-04-16 22:25:41 -0700

    bump to SNAPSHOT

--- elephantdb-leveldb/project.clj
+++ elephantdb-leveldb/project.clj
@@ -1,9 +1,9 @@
-(defproject elephantdb/elephantdb-leveldb "0.4.2"
+(defproject elephantdb/elephantdb-leveldb "0.4.3-SNAPSHOT"
   :min-lein-version "2.0.0"
   :java-source-paths ["src/jvm"]
   :javac-options ["-source" "1.6" "-target" "1.6"]
   :repositories {"fusesource.nexus.snapshot" "http://repo.fusesource.com/nexus/content/groups/public-snapshots"}
-  :dependencies [[elephantdb/elephantdb-core "0.4.2"]
+  :dependencies [[elephantdb/elephantdb-core "0.4.3-SNAPSHOT"]
                  [org.fusesource.leveldbjni/leveldbjni-all "1.5"]]
   :profiles {:dev
              {:dependencies
