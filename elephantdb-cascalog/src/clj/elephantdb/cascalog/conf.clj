7369ced153aad84be5d26f7de7451460d3ba8ff4
Soren Macbeth <soren@dopeness.org>
2013-04-24 20:31:50 -0700

    Add ability to set persistence options

--- elephantdb-cascalog/src/clj/elephantdb/cascalog/conf.clj
+++ elephantdb-cascalog/src/clj/elephantdb/cascalog/conf.clj
@@ -1,18 +1,22 @@
 (ns elephantdb.cascalog.conf
   (:require [cascalog.workflow :as w])
-  (:import [elephantdb DomainSpec]
+  (:import [elephantdb DomainSpec DomainSpec$Args]
            [elephantdb.cascading ElephantDBTap$Args]
            [java.util ArrayList]))
 
 (defn convert-java-domain-spec [^DomainSpec spec]
   {:coordinator  (.getCoordinator spec)
    :shard-scheme (.getShardScheme spec)
-   :num-shards   (.getNumShards spec)})
+   :num-shards   (.getNumShards spec)
+   :persistence-options (.getPersistenceOptions spec)})
 
 (defn convert-clj-domain-spec
-  [{:keys [coordinator shard-scheme num-shards]}]
+  [{:keys [coordinator shard-scheme num-shards persistence-options]}]
   {:pre [(and coordinator shard-scheme num-shards)]}
-  (DomainSpec. coordinator shard-scheme num-shards))
+  (when persistence-options
+    (let [args (DomainSpec$Args.)]
+      (set! (.persistenceOptions args) persistence-options)
+      (DomainSpec. coordinator shard-scheme num-shards args))))
 
 (defn read-domain-spec
   "A domain spec is stored with shards in the VersionedStore. Look to
